0ab201526a61b7a70996722301525d2d97a3ce8a
peterbonnesoeur <maxime.bonn@gmail.com>
2022-06-21 17:57:14 +0200

    Added script to track the progress

---
+++ .github/scripts/generate_progress.sh
@@ -0,0 +1,7 @@
+alias yo='find . -type f -name "*.py" | wc -l'
+
+alias completion_rate='expr $(expr $(yo) \*  100) / 150'
+
+alias progress_bar='echo "![Progress](https:\/\/progress-bar.dev\/$(completion_rate)\/)"'
+
+sed -i '' "5s/.*/$(progress_bar)/" README.md
