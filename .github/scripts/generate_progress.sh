27fa93d19edbf14e9f869dc4396b7cb5408f1b69
Maxime Bonnesoeur <44313994+peterbonnesoeur@users.noreply.github.com>
2022-06-22 09:21:46 +0200

    Update generate_progress.sh
    Updated the file to work on linux distributions

--- .github/scripts/generate_progress.sh
+++ .github/scripts/generate_progress.sh
@@ -6,4 +6,4 @@ alias completion_rate='expr $(expr $(yo) \*  100) / 150'
 
 alias progress_bar='echo "![Progress](https:\/\/progress-bar.dev\/$(completion_rate)\/)"'
 
-sed -i '' "5s/.*/$(progress_bar)/" README.md
+sed -i "5s/.*/$(progress_bar)/" README.md
