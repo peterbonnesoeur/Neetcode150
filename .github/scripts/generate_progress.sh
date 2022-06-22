40bb0a33f7cfdff4c201e35344ceb0e556c5fa4b
peterbonnesoeur <maxime.bonn@gmail.com>
2022-06-22 10:59:46 +0200

    Updated the script to run on MacOS and linux

--- .github/scripts/generate_progress.sh
+++ .github/scripts/generate_progress.sh
@@ -6,4 +6,8 @@ alias completion_rate='expr $(expr $(yo) \*  100) / 150'
 
 alias progress_bar='echo "![Progress](https:\/\/progress-bar.dev\/$(completion_rate)\/)"'
 
-sed -i "5s/.*/$(progress_bar)/" README.md
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "5s/.*/$(progress_bar)/" README.md
+else
+  sed -i "5s/.*/$(progress_bar)/" README.md
+fi
