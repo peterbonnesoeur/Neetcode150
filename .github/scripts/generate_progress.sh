addfa5294fe4edd5e9b43a493a8af7a8642a5991
peterbonnesoeur <maxime.bonn@gmail.com>
2024-10-01 23:14:28 +0200

    Updated completion Rate github action

--- .github/scripts/generate_progress.sh
+++ .github/scripts/generate_progress.sh
@@ -2,9 +2,10 @@ shopt -s expand_aliases
 
 alias yo='find . -type f -name "*.py" | wc -l'
 
-alias completion_rate='expr $(expr $(yo) \*  100) / 150'
+alias completion_rate='rate=$(expr $(expr $(yo) \*  100) / 150); if [ "$rate" -gt 100 ]; then echo 100; else echo "$rate"; fi'
 
-alias progress_bar='echo "![Progress](https:\/\/progress-bar.dev\/$(completion_rate)\/)"'
+alias progress_bar='echo "![Progress](https:\/\/geps.dev\/progress\/$(completion_rate)\/)"'
+
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' "5s/.*/$(progress_bar)/" README.md
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Progression
 
-![Progress](https://progress-bar.dev/101/)
+![Progress](https://geps.dev/progress/100/)
 
 ## Exercises to redo:
 
